4e9ca99b361998f1caa2b0160f40316d4a0fd080
Son Nguyen <nhson219@gmail.com>
2021-03-23 19:33:07 +0700

    lifecycle

--- ExerciseProject/app/src/main/java/com/example/exerciseproject/MainActivity.java
+++ ExerciseProject/app/src/main/java/com/example/exerciseproject/MainActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +17,44 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
     private int duration = Toast.LENGTH_SHORT;
 
+    //PLUMBING: Pairing GUI controls with Java objects
+    private Button btnExit;
+    private EditText txtColorSelected;
+    private TextView txtSpyBox;
+    private LinearLayout myScreen;
+    private String PREFNAME = "myPrefFile1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        txtColorSelected = (EditText) findViewById(R.id.editText1);
+        btnExit = (Button) findViewById(R.id.button1);
+        txtSpyBox = (TextView) findViewById(R.id.textView1);
+        myScreen = (LinearLayout) findViewById(R.id.myScreen1);
+
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+//        txtColorSelected.addTextChangedListener(new TextWatcher() {
+//            @Override
+//        public void onTextChanged(CharSequence s, int start, int before, int count) { // nothing TODO, needed by interface
+//        }
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//// nothing TODO, needed by interface }
+//                @Override
+//                public void afterTextChanged(Editable s) {
+////set background to selected color
+//                    String chosenColor = s.toString().toLowerCase(Locale.US); txtSpyBox.setText(chosenColor); setBackgroundColor(chosenColor, myScreen);
+//                } });
         context = getApplicationContext();
         Toast.makeText(context, "onCreate", duration).show();
 
